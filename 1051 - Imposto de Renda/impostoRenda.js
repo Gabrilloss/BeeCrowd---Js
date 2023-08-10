ba00eeb199a9350779178dad38e565a16cc4fe5a
Gabrillos <gabrielluizdacruzignacio@gmail.com>
2023-08-10 08:53:25 -0300

    resolvido

---
+++ 1051 - Imposto de Renda/impostoRenda.js
@@ -0,0 +1,24 @@
+var input = require('fs').readFileSync('./1051 - Imposto de Renda/stdin', 'utf8');
+var lines = input.split('\n');
+/*  Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e mostre o valor que esta pessoa deve pagar de Imposto de Renda.
+
+    Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. No exemplo fornecido (abaixo), a taxa é de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em R$ 80.36 no total. O valor deve ser impresso com duas casas decimais.
+
+Entrada
+A entrada contém apenas um valor de ponto flutuante, com duas casas decimais.
+
+Saída
+Imprima o texto "R$" seguido de um espaço e do valor total devido de Imposto de Renda, com duas casas após o ponto. Se o valor de entrada for menor ou igual a 2000, deverá ser impressa a mensagem "Isento". */
+const salario = lines[0];
+if ( salario >= 0 && salario <= 2000.00){
+    console.log('Isento');
+}
+else if (salario >= 2000.01 && salario <= 3000.00){
+    console.log(`R$ ${((parseFloat(salario) - 2000.00) * 0.08).toFixed(2)}`);
+}
+else if (salario >= 3000.01 && salario <= 4500.00){
+    console.log(`R$ ${(((parseFloat(salario) - 3000.00) * 0.18) + (1000.00 * 0.08)).toFixed(2)}`);
+}
+else if (salario > 4500.00){
+    console.log(`R$ ${(((parseFloat(salario) - 4500.00) * 0.28) + (1500.00 * 0.18) + (1000.00 * 0.08)).toFixed(2)}`);
+}
