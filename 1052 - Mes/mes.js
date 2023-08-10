576c38a0384c841b8a7d98ce721c00ba4af3570e
Gabrillos <gabrielluizdacruzignacio@gmail.com>
2023-08-10 09:05:01 -0300

    resolvido

---
+++ 1052 - Mes/mes.js
@@ -0,0 +1,22 @@
+var input = require('fs').readFileSync('./1052 - Mes/stdin', 'utf8');
+var lines = input.split('\n');
+/* Leia um valor inteiro entre 1 e 12, inclusive. Correspondente a este valor, deve ser apresentado como resposta o mês do ano por extenso, em inglês, com a primeira letra maiúscula.
+
+Entrada
+A entrada contém um único valor inteiro.
+
+Saída
+Imprima por extenso o nome do mês correspondente ao número existente na entrada, com a primeira letra em maiúscula. */
+const mes = lines[0];
+if (mes == 1 ) {console.log('January')}
+else if(mes == 2){console.log('February')}
+else if(mes == 3){console.log('March')}
+else if(mes == 4){console.log('April')}
+else if(mes == 5){console.log('May')}
+else if(mes == 6){console.log('June')}
+else if(mes == 7){console.log('July')}
+else if(mes == 8){console.log('August')}
+else if(mes == 9){console.log('September')}
+else if(mes == 10){console.log('October')}
+else if(mes == 11){console.log('November')}
+else {console.log('December')}
